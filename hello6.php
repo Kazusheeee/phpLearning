fc1a86e2f9223e9aceaae643aa3ca8cbf076547e
kazushi <kaz7ats@gmail.com>
2020-07-14 15:06:59 +0900

    配列専用の繰り返し構文を追加

--- hello6.php
+++ hello6.php
@@ -2,8 +2,11 @@
 $total = 0;
 $data = [98, 76, 54, 32, 19, 87, 65, 43, 21];
 $n = count($data);
-for($i = 0; $n; $i++){
-    $total += $data[$i];
+#for($i = 0; $n; $i++){
+    #$total += $data[$i];
+#}
+foreach($data as $item){
+    $total += $item;
 }
 $ave = $total / $n;
 echo "合計:" . $total . "、平均:" . $ave;
