c88a4ee98be7db96435bb44de576c2aa296b97db
kazushi <kaz7ats@gmail.com>
2020-07-14 14:57:31 +0900

    配列の練習

---
+++ hello6.php
@@ -0,0 +1,9 @@
+<?php
+$total = 0;
+$data = [98, 76, 54, 32, 19, 87, 65, 43, 21];
+$n = count($data);
+for($i = 0; $n; $i++){
+    $total += $data[$i];
+}
+$ave = $total / $n;
+echo "合計:" . $total . "、平均:" . $ave;
